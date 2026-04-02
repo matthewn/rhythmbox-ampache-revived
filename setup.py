8b728adc8d2afc6205adfb3349031f4bb8413bd2
Matthew Newton <matthewn@mahnamahna.net>
2026-04-02 14:17:46 -0700

    modernize setup.py

--- setup.py
+++ setup.py
@@ -1,45 +1,54 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-from distutils.core import setup
-from distutils.command.install_data import install_data
 import os
+import subprocess
 import sys
 
+from setuptools import setup
+from setuptools.command.install import install
 
-class post_install(install_data):
+
+class PostInstall(install):
+    """Compile GLib schemas after installing data files."""
+
     def run(self):
-        # Call parent
-        install_data.run(self)
-
+        super().run()
         if glib_compile_schemas:
-            # Execute commands after copying
-            os.system('glib-compile-schemas %s/share/glib-2.0/schemas' % self.install_dir)
+            schemas_dir = os.path.join(
+                self.install_base, 'share', 'glib-2.0', 'schemas')
+            subprocess.run(['glib-compile-schemas', schemas_dir], check=False)
 
 
-# determine lib path depending on architecture
-if (os.uname()[4].find('64') != -1 or os.uname()[4].find('s390x') != -1):
-    lib_dir = 'lib64'
-else:
-    lib_dir = 'lib'
-
-# optionally compile glib schemas
 glib_compile_schemas = True
 
-if "--no-glib-compile-schemas" in sys.argv:
+if '--no-glib-compile-schemas' in sys.argv:
     glib_compile_schemas = False
-    sys.argv.remove("--no-glib-compile-schemas")
+    sys.argv.remove('--no-glib-compile-schemas')
 
-setup(name="rhythmbox-ampache",
-      cmdclass={"install_data": post_install},
-      version="0.11",
-      description="A Rhythmbox plugin to stream music from an Ampache server",
-      author="Rhythmbox Ampache plugin team",
-      author_email="rhythmbox-ampache@googlegroups.com",
-      url="http://code.google.com/p/rhythmbox-ampache",
-      packages=[],
-      data_files=[
-          (lib_dir + "/rhythmbox/plugins/ampache", ["ampache.plugin", "ampache.py", "AmpacheBrowser.py", "AmpacheConfigDialog.py"]),
-          ("share/rhythmbox/plugins/ampache", ["ampache-prefs.ui", "ampache.ico", "ampache.png"]),
-          ("share/glib-2.0/schemas", ["org.gnome.rhythmbox.plugins.ampache.gschema.xml"]),
-          ],
-      )
+
+setup(
+    name='rhythmbox-ampache',
+    cmdclass={'install': PostInstall},
+    version='0.12',
+    description='A Rhythmbox plugin to stream music from an Ampache server',
+    author='Rhythmbox Ampache plugin team',
+    author_email='rhythmbox-ampache@googlegroups.com',
+    url='https://github.com/lotan/rhythmbox-ampache',
+    packages=[],
+    data_files=[
+        ('lib/rhythmbox/plugins/ampache', [
+            'ampache.plugin',
+            'ampache.py',
+            'AmpacheBrowser.py',
+            'AmpacheConfigDialog.py',
+        ]),
+        ('share/rhythmbox/plugins/ampache', [
+            'ampache-prefs.ui',
+            'ampache.ico',
+            'ampache.png',
+        ]),
+        ('share/glib-2.0/schemas', [
+            'org.gnome.rhythmbox.plugins.ampache.gschema.xml',
+        ]),
+    ],
+)
